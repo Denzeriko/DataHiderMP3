193bd96723f8ac5900e942d89895b2e4ead48a8d
Denz <denzeriko@gmail.com>
2021-12-06 23:09:40 +0300

    Hash system to check password is correct. Minor fixes.

--- mp3_filehider/fileManipulator.h
+++ mp3_filehider/fileManipulator.h
@@ -8,15 +8,17 @@
 
 class FILESYS {
 public:
-	std::string delimiter = "VYBQ@!^BEAW&D^";  //random bytes to get delimer in decoding
+	std::string delimiter = "RprjH74i94Wf4RFrW5EP!$@";  //random bytes to get delimer in decoding
 
-	int init() {
+	FILESYS() {
 		printf("\n:::::::::::::::::::\n");
 		printf("::DENZ MP3 AES128::\n");
 		printf("::      2021     ::\n");
 		printf(":::::::::::::::::::\n\n");
-		printf("FILESYS: OK!\n\n");
-		return 1;
+		printf("FILESYS: CREATED!\n\n");
+	}
+	~FILESYS() {
+		printf("\nFILESYS: DELETED!\n\n");
 	}
 
 	void fixKey(char* key, char* _key) {
@@ -35,7 +37,7 @@ public:
 	}
 
 	int checkData(char* mainFile) {
-		printf("Reading: %s\n", mainFile);
+		printf("PASS #1 Reading: %s\n", mainFile);
 		std::ifstream as(mainFile, std::ios::out | std::ios::binary);
 		std::stringstream ss;
 		ss << as.rdbuf();
@@ -46,10 +48,10 @@ public:
 		while ((pos = s.find(delimiter)) != std::string::npos) {
 			token = s.substr(0, pos);
 			s.erase(0, pos + delimiter.length());
-			printf("Some data was found!\n");
+			printf("Signature found!\n");
 			return 0;
 		}
-		printf("No data was found!\n");
+		printf("ERR# (No signature was found!)\n");
 		return 1;
 	}
 
@@ -59,29 +61,34 @@ public:
 
 		printf("::ENCODE::\n");
 
+		std::string hashStr{ _key }; //It's not work storing key or using key as hash in encode. 
+		std::hash<std::string> hasher; //Just to check main key. I guess it's bad idea, but pretty cool feature.
+		unsigned int hashResult = hasher(hashStr) + hasher(hashStr);
+
 		//file container
 		printf("PASS #1 Reading %s ", mainFile);
-		std::ifstream mainStream(mainFile, std::ios::out | std::ios::binary);
+		std::ifstream mainStream(mainFile, std::ios::in | std::ios::binary);
 		if (!mainStream.good()) {
 			printf("ERR!\n");
 			return;
 		}
 		std::stringstream mainBuffer;
-		mainBuffer << mainStream.rdbuf() << delimiter;
+		mainBuffer << mainStream.rdbuf() << delimiter << hashResult << hashResult;
+		mainStream.close();
 		printf("OK!\n");
 
 		//What to write
 		printf("PASS #2 Reading %s ", dataFile);
-		std::ifstream dataStream(dataFile, std::ios::out | std::ios::binary);
+		std::ifstream dataStream(dataFile, std::ios::in | std::ios::binary);
 		if (!dataStream.good()) {
 			printf("ERR!\n");
 			return;
 		}	
 		std::stringstream dataBuffer;
 		dataBuffer << dataStream.rdbuf();
+		dataStream.close();
 		printf("OK!\n");
 
-		
 		uLong src_len = dataBuffer.str().size();
 		uLong cmp_len = compressBound(src_len);
 		printf("PASS #3 Compressing data %u to ", src_len);
@@ -119,25 +126,51 @@ public:
 
 		printf("::DECODE::\n");
 
+		std::string hashStr{ _key };
+		std::hash<std::string> hasher;
+		unsigned int hashResult = hasher(hashStr) + hasher(hashStr);
+
 		printf("PASS #1 Read file ");
-		std::ifstream mainStream(mainFile, std::ios::out | std::ios::binary);
+		std::ifstream mainStream(mainFile, std::ios::in | std::ios::binary);
 		if (!mainStream.good()) {
 			printf("ERR!\n");
 			return;
 		}
 		std::stringstream mainBuffer;
 		mainBuffer << mainStream.rdbuf();
+		mainStream.close();
 		printf("OK!\n");
 		
 		printf("PASS #2 Searching for signature ");
 		size_t pos = 0;
 		std::string token;
 		std::string mainBufferS = mainBuffer.str();
-		while ((pos = mainBufferS.find(delimiter)) != std::string::npos) {
+
+		bool checkSignature = false;
+		bool checkKeyPassword = false;
+		while ((pos = mainBufferS.find(delimiter)) != std::string::npos) { //todo: rework this part...
 			token = mainBufferS.substr(0, pos);
-			mainBufferS.erase(0, pos + delimiter.length());
+			checkSignature = true;
+			mainBufferS.erase(0, pos + std::to_string(hashResult).length());
 		}
+		if (!checkSignature)
+		{
+			printf("ERR# (No signature was found!)\n");
+			return;
+		}
+		while ((pos = mainBufferS.find(std::to_string(hashResult))) != std::string::npos) {
+			token = mainBufferS.substr(0, pos);
+			checkKeyPassword = true;
+			mainBufferS.erase(0, pos + std::to_string(hashResult).length());
+		}
+		if (!checkKeyPassword)
+		{
+			printf("ERR# (Wrong password!)\n");
+			return;
+		}
+		
 		printf("OK!\n");
+		//printf("OK! %d | %d\n", checkSignature, checkKeyPassword);
 
 		printf("PASS #3 Decrypting data ");
 		AES aes(128);
@@ -146,7 +179,7 @@ public:
 		printf("OK!\n");
 
 		printf("PASS #4 Decompressing data ");
-		uLong src_len = 100000000;// mainBufferS.size(); // HOW THE FUCK I MUST KNOW ORIGINAL SIZE?
+		uLong src_len = 100000000;//mainBufferS.size(); // HOW I MUST KNOW ORIGINAL SIZE?
 		uLong uncomp_len = src_len;
 		unsigned char* pUncomp = (mz_uint8*)malloc((size_t)src_len);
 		int comp_status = uncompress(pUncomp, &uncomp_len, out, src_len);
--- mp3_filehider/main.cpp
+++ mp3_filehider/main.cpp
@@ -11,25 +11,24 @@
 
 //unsigned char key[16] = "XXXXXXXXXXXXXXX"; key must look like that
 //you can use any length to 16
-                           
+
 int main(int argc, char** argv) {
 	if (argc == 1)
 		return 1;
 
-	FILESYS filesys;
-	filesys.init();
-
+	FILESYS* filesys = new FILESYS;
 	switch (*argv[1]) {
 		case 'e':
-			filesys.encodeInput(argv[2], argv[3], argv[4]);
+			filesys->encodeInput(argv[2], argv[3], argv[4]);
 			break;
 		case 'd':
-			filesys.decodeInput(argv[2], argv[3], argv[4]);
+			filesys->decodeInput(argv[2], argv[3], argv[4]);
 			break;
 		case 'c':
-			filesys.checkData(argv[2]);
+			filesys->checkData(argv[2]);
 			break;
 	}
-	
+	delete filesys;
+
 	return 0;
 }
